1c624ec2c1edee3570b9f89f7e4dd9848378b61d
vikram gauda <bikram-spec>
2021-04-22 21:17:09 +0000

    chart modified

--- src/app/client/statics/statics.component.ts
+++ src/app/client/statics/statics.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {WebRequestsService } from '../../services/web-requests.service';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-statics',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaticsComponent implements OnInit {
 
-  constructor() { }
+  uri:string;
+  token:string;
+  trustedDashboardUrl;
+  iframeuri;
+
+  constructor(private webreq:WebRequestsService,private storage:LocalStorageService,private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+
+    this.token=this.storage.getToken("authorization");
+    console.log(this.token);
+    this.uri=`http://localhost:3000/engine/cchart/${this.token}`;
+    this.iframeuri = this.sanitizer.bypassSecurityTrustResourceUrl(this.uri);
+    console.log(this.uri);      
+
   }
 
 }
